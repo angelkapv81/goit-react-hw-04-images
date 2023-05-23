05bd347d74eb49bcc4eb448d398ebc1dbd29ae84
angelkapv81@gmail.com <angelkapv81>
2023-05-24 00:40:15 +0300

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { ModalOverlay, ModalContainer } from './Modal.styled';
 
 const modalRoot = document.getElementById('modal-root');
 
-export default class Modal extends Component {
-  // propTypes
-  static propTypes = {
-    selectedImage: PropTypes.string,
-    tags: PropTypes.string,
-    onClose: PropTypes.func,
-  };
+const Modal = ({ selectedImage, tags, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    const { selectedImage, tags } = this.props;
 
-    return createPortal(
-      <ModalOverlay onClick={this.handleBackdropClick}>
-        <ModalContainer>
-          <img src={selectedImage} alt={tags} />
-        </ModalContainer>
-      </ModalOverlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <ModalOverlay onClick={handleBackdropClick}>
+      <ModalContainer>
+        <img src={selectedImage} alt={tags} />
+      </ModalContainer>
+    </ModalOverlay>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  selectedImage: PropTypes.string,
+  tags: PropTypes.string,
+  onClose: PropTypes.func,
+};
+
+export default Modal;
